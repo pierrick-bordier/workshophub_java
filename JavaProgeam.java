07f2c93cb0f2ebe73861763bd1cff27cb4722790
Pierrick <pierrick.bordier@epitech.eu>
2023-12-11 10:43:55 +0400

    finished ex 1

---
+++ JavaProgeam.java
@@ -0,0 +1,8 @@
+public class JavaProgeam {
+    public static void main(String []args) {
+        Vehicle thing = new Vehicle("Volkswagen", "Scirocco", 2008);
+        thing.Accelerate();
+        thing.Brake();
+        thing.Describe();
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,20 @@
+
+public class Vehicle {
+    String Brand;
+    String Model;
+    int Year;
+    public Vehicle(String Brand1, String Model1, int Year1) {
+        Brand = Brand1;
+        Model = Model1;
+        Year = Year1;
+    }
+    public void Accelerate() {
+        System.out.printf("I'm putting the pedal to the metal\n");
+    }
+    public void Brake() {
+        System.out.printf("Woah! Slow down boy!\n");
+    }
+    public void Describe() {
+        System.out.printf("I'm a %s %s made in %d\n", Brand, Model, Year);
+    }
+}
